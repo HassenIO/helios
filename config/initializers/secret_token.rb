6117b690f7ec4fc97c9e786e9ddd60de8c6c00a6
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-03-21 15:22:48 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelerCar::Application.config.secret_token = '0b980c59d93a753c479bfdca231bd3f6143d58475d6df575e7b3826cda1e7d79412a49994e3095493f354b3d122d54428cfeabc9b1ea5e6b5283bdf46ff9da8a'
