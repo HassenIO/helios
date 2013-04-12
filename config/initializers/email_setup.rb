8e57b45b8d047cc3f60b52be66ff7525ba0bbfb7
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-04-12 17:53:16 +0200

    test email via heroku/gmail

---
+++ config/initializers/email_setup.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "test.smtp@adventori.com",
+    :password => "test;smtp",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:host] = ENV['HOST'] # Your app URL. E.g: myapp.herokuapp.com
