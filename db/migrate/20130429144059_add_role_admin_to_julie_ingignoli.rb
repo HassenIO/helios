2218d79a67e819eb9c2d9e9aea921b88f8be161a
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-04-29 16:45:05 +0200

    Add admin role to julie.ingignoli@gmail.com

---
+++ db/migrate/20130429144059_add_role_admin_to_julie_ingignoli.rb
@@ -0,0 +1,10 @@
+class AddRoleAdminToJulieIngignoli < ActiveRecord::Migration
+  def change
+    user = User.find_by_email("julie.ingignoli@gmail.com")
+
+    if user && (!user.has_role? :admin)
+      user.addRole("admin")
+    end
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130425150856) do
+ActiveRecord::Schema.define(:version => 20130429144059) do
 
   create_table "air_ports", :force => true do |t|
     t.string "name"
