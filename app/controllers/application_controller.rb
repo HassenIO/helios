70a061a138b48e04f2ba6cfef9282cf60287d3cf
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-05-03 16:06:20 +0200

    Fixing routes with locale like REST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,9 +12,8 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options(options={})
-    logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { :locale => I18n.locale }
+  def url_options
+    {:locale => I18n.locale}.merge(super)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ TravelerCar::Application.routes.draw do
 
   root :controller => "home", :action => "index"
 
-  scope "/:locale" do
+  scope "/:locale", :constraints => {:locale=> /[a-z]{2}(-[A-Z]{2})?/ } do
+
+    root :controller => "home", :action => "index"
 
     devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
 
@@ -18,8 +20,6 @@ TravelerCar::Application.routes.draw do
       resources :travels
     end
 
-
-
     authenticated :user do
       get "/travels", to: redirect { |p, req| "/#{p[:locale]}/users/#{req.env["warden"].user(:user).id}/travels/new" }
     end
@@ -32,8 +32,6 @@ TravelerCar::Application.routes.draw do
   end
 
 
-  get "/travels", to: redirect("/"+I18n.locale.to_s+'/travels')
 
-  get "/search", to: redirect("/"+I18n.locale.to_s+'/search')
 
 end
