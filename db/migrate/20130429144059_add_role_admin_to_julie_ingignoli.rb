676760e87cd4882404276ff589f3b8d6dd58f2c2
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-05-14 14:16:01 +0200

    Fix addRole missing method

--- db/migrate/20130429144059_add_role_admin_to_julie_ingignoli.rb
+++ db/migrate/20130429144059_add_role_admin_to_julie_ingignoli.rb
@@ -2,9 +2,9 @@ class AddRoleAdminToJulieIngignoli < ActiveRecord::Migration
   def change
     user = User.find_by_email("julie.ingignoli@gmail.com")
 
-    if user && (!user.has_role? :admin)
-      user.addRole("admin")
-    end
+    #if user && (!user.has_role? :admin)
+    #  user.addRole("admin")
+    #end
 
   end
 end
