1fe33913ecc4d6c703dd52a2452bd66bf3a22850
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-05-15 23:28:38 +0200

    Spit date in two fields date and time

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -4,19 +4,19 @@ class RentPeriodValidator < ActiveModel::Validator
 
     unless record.startDate.blank?
       unless record.startDate > Date.today
-        record.errors.add(:startDate,:cannot_be_past)
+        record.errors.add(:startDate, :cannot_be_past)
       end
     end
 
     unless record.endDate.blank?
       unless record.endDate > Date.today
-        record.errors.add(:endDate,:cannot_be_past)
+        record.errors.add(:endDate, :cannot_be_past)
       end
     end
 
     unless record.startDate.blank? or record.endDate.blank?
       unless record.startDate < record.endDate
-        record.errors.add(:endDate,:cannot_be_before_start)
+        record.errors.add(:endDate, :cannot_be_before_start)
       end
     end
   end
@@ -25,7 +25,11 @@ end
 class Rent < ActiveRecord::Base
   include ActiveModel::Validations
 
-  attr_accessible :endDate, :startDate, :travel_id, :driver_attributes, :user_id, :airPort_id, :has_accepted_cgv
+  attr_accessible :endDate, :endDate_time, :endDate_date, :startDate, :startDate_time, :startDate_date, :travel_id,
+                  :driver_attributes, :user_id, :airPort_id, :has_accepted_cgv
+
+  # add the accessors for the two fields
+  attr_accessor :startDate_time, :startDate_date, :endDate_time, :endDate_date
 
 
   belongs_to :user
@@ -48,12 +52,33 @@ class Rent < ActiveRecord::Base
 
   validates_associated :driver
 
+  # add some callbacks
+  after_initialize :get_datetimes # convert db format to accessors
+  before_validation :set_datetimes # convert accessors back to db format
+
   def self.valid_attribute?(attrib, value)
     mock = self.new(attrib => value)
     unless mock.valid?
       return !mock.errors.get(attrib).present?
     end
     true
+  end
+
+  def get_datetimes
+    self.startDate ||= Time.now + 2.days # if the startDate time is not set, set it to now
+
+    self.startDate_date ||= self.startDate.to_date.to_s(:db) # extract the date is yyyy-mm-dd format
+    self.startDate_time ||= "#{'%02d' % self.startDate.hour}:#{'%02d' % self.startDate.min}" # extract the time
+
+    self.endDate ||= Time.now + 9.days # if the endDate time is not set, set it to now
+
+    self.endDate_date ||= self.endDate.to_date.to_s(:db) # extract the date is yyyy-mm-dd format
+    self.endDate_time ||= "#{'%02d' % self.endDate.hour}:#{'%02d' % self.endDate.min}" # extract the time
+  end
+
+  def set_datetimes
+    self.startDate = "#{self.startDate_date} #{self.startDate_time}:00" # convert the two fields back to db
+    self.endDate = "#{self.endDate_date} #{self.endDate_time}:00" # convert the two fields back to db
   end
 
 end
