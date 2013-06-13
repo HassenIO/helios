5308b282209a879daf4c204eb4710306888d71a1
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-06-13 18:00:35 +0200

    Add transmission to car + fix default value on car attributes

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,6 @@
 class Car < ActiveRecord::Base
 
-  attr_accessible :brand, :model, :fuel, :nbSeats, :km, :year, :license , :desc, :category_id
+  attr_accessible :brand, :model, :fuel, :nbSeats, :km, :year, :license , :desc, :category_id, :transmission
   attr_accessible :hasChildSeat, :hasGps, :hasCarRadio, :isSmoker, :acceptedPets, :hasAirConditioning
   attr_accessible :filepicker1_url, :filepicker2_url, :filepicker3_url
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130604134201) do
+ActiveRecord::Schema.define(:version => 20130613152123) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(:version => 20130604134201) do
     t.string   "filepicker2_url"
     t.string   "filepicker3_url"
     t.integer  "category_id"
+    t.string   "transmission"
   end
 
   create_table "categories", :force => true do |t|
