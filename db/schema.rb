6215a1f2e20d1694762636aae855a2b5c80b4783
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-06-14 10:40:19 +0200

    Fix #49 - Dans le listing des voitures disponibles pour un locataire, supprimer l'affichage des commentaires et rajouter plutôt le nombre de km, le carburant,  le GPS, automatique/manuelle

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130613152123) do
+ActiveRecord::Schema.define(:version => 20130614075427) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
