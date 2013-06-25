d1d11b4556ff3211aac77c80eb702a95939c47ae
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-06-25 12:51:54 +0200

    Add scope for production deployment with wordpress

--- config/routes.rb
+++ config/routes.rb
@@ -2,39 +2,42 @@ TravelerCar::Application.routes.draw do
 
   root :controller => "home", :action => "index"
 
-  match  "notifications" => "notifications#index"
 
-  scope "/:locale", :constraints => {:locale=> /[a-z]{2}(-[A-Z]{2})?/ } do
+  scope "/a" do
+    match "notifications" => "notifications#index"
 
-    root :controller => "home", :action => "index"
+    scope "/:locale", :constraints => {:locale => /[a-z]{2}(-[A-Z]{2})?/} do
 
-    devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+      root :controller => "home", :action => "index"
 
-    resources :users do
-      resources :travels do
-        get 'cgv', :on => :member
+      devise_for :users, :controllers => {:omniauth_callbacks => "users/omniauth_callbacks"}
+
+      resources :users do
+        resources :travels do
+          get 'cgv', :on => :member
+        end
+        resources :cars
+        resources :rents do
+          resource :payment
+          get 'cgv', :on => :member
+        end
       end
-      resources :cars
-      resources :rents do
-        resource :payment
-        get 'cgv', :on => :member
+      resources :rents, only: [:new]
+      resources :travels, only: [:new]
+
+      authenticated :user do
+        match "/travels", to: redirect { |p, req| "/a/#{p[:locale]}/users/#{req.env["warden"].user(:user).id}/travels/new" }, :as => 'travels'
       end
-    end
-    resources :rents, only: [:new]
-    resources :travels, only: [:new]
 
-    authenticated :user do
-      match "/travels", to: redirect { |p, req| "/#{p[:locale]}/users/#{req.env["warden"].user(:user).id}/travels/new" }  , :as => 'travels'
-    end
+      unauthenticated :user do
+        match '/travels(/:action)' => 'anonymous_travels', :as => 'travels'
+      end
 
-    unauthenticated :user do
-      match '/travels(/:action)' => 'anonymous_travels', :as => 'travels'
+      match '/search(/:action)' => 'search', :as => 'search'
+
     end
 
-    match '/search(/:action)' => 'search' , :as => 'search'
-
+    ActiveAdmin.routes(self)
   end
-
-  ActiveAdmin.routes(self)
 
 end
