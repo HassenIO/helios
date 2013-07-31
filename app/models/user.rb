3c15ae8e4ed76fa17a657be0a15a26cea6b75436
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-07-31 17:21:30 +0200

    Fix connection with Facebook  when user email already exist

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,14 +49,30 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
-      user = User.create(first_name: auth.info.first_name,
-                         last_name: auth.info.last_name,
-                         provider: auth.provider,
-                         uid: auth.uid,
-                         birth_date: auth.extra.raw_info.birthday,
-                         email: auth.info.email,
-                         password: Devise.friendly_token[0, 20]
-      )
+      #associate if email exist but not a facebook one
+      user = User.find_by_email(auth.info.email)
+
+      if user
+        user.birth_date ||= auth.extra.raw_info.birthday
+        user.first_name ||= auth.info.first_name
+        user.last_name ||= auth.info.last_name
+        user.provider ||= auth.provider
+        user.uid ||= auth.uid
+
+        user.save
+
+      else
+        user = User.create(first_name: auth.info.first_name,
+                           last_name: auth.info.last_name,
+                           provider: auth.provider,
+                           uid: auth.uid,
+                           birth_date: auth.extra.raw_info.birthday,
+                           email: auth.info.email,
+                           password: Devise.friendly_token[0, 20]
+        )
+      end
+
+
     end
     user
   end
