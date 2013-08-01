6e03d9078ca874ae7f9fe97aa753fa3689adb95b
Julie Ingignoli <julie.ingignoli@novacodex.net>
2013-08-01 17:03:07 +0200

    Fix km display

--- app/helpers/cars_helper.rb
+++ app/helpers/cars_helper.rb
@@ -1,2 +1,13 @@
 module CarsHelper
+
+  CONVERTER = {0 => '0-50000 km', 50000 => '50000-100000 km', 100000 => '100000-150000 km', 150000 => '150000 km et plus'}
+
+  def km_to_human(km)
+    CONVERTER[km]
+  end
+
+  def km_to_list
+    CONVERTER.collect {|k,v| [v,k]}
+  end
+
 end
