714962a46a74cfb6636cdeeead6ef847ac347ef5
Hassen Taidirt <htaidirt@gmail.com>
2013-08-31 00:16:47 +0200

    Config Mailer using SendGrid params.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -6,11 +6,20 @@ if  Rails.env.production?
   }
 else
   ActionMailer::Base.smtp_settings ={
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :user_name => "test.smtp@adventori.com",
-      :password => "smtp;test",
+      # :address => "smtp.gmail.com",
+      # :port => 587,
+      # :user_name => "test.smtp@adventori.com",
+      # :password => "smtp;test",
+      # :authentication => "plain",
+      # :enable_starttls_auto => true,
+
+      # Using SendGrid for Heroku. Source: https://devcenter.heroku.com/articles/sendgrid
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
       :authentication => "plain",
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'travelercar-test.heroku.com',
       :enable_starttls_auto => true
   }
 end
@@ -24,4 +33,4 @@ if Rails.env.development?
   AdminMailer.register_interceptor(OverrideMailRecipient)
 end
 
-ActionMailer::Base.default_url_options = {:host => ENV['HOST'], :locale => I18n.locale} # Your app URL. E.g: myapp.herokuapp.com
+# ActionMailer::Base.default_url_options = {:host => ENV['HOST'], :locale => I18n.locale} # Your app URL. E.g: myapp.herokuapp.com
