d0bd3d5de2fb4c9c089c4f0cf992d8b538e4d07f
Hassen Taidirt <htaidirt@gmail.com>
2013-08-30 23:48:39 +0200

    Try to config SendGrid to send mails from Heroku.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ TravelerCar::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Define ActionMailer URL host
+  config.action_mailer.default_url_options = { :host => 'travelercar-test.heroku.com' }
+
 end
