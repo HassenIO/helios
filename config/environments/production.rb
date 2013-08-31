e2cfe009e5a2cb54d44b39282894df3a3717563b
Hassen Taidirt <htaidirt@gmail.com>
2013-08-31 13:18:20 +0200

    Set production assets compile to true to avoid Heroku error: Page not found.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ TravelerCar::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # Nota: Set to true to avoid Heroku error (source: http://stackoverflow.com/questions/7376966/heroku-problem-the-page-you-were-looking-for-doesnt-exist)
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
