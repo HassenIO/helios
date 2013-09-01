804d4cd6d2591eb1bbd0057346875b2381f59b00
Hassen Taidirt <htaidirt@gmail.com>
2013-09-01 13:34:06 +0200

    Add :locale to generate URL.

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,17 +1,18 @@
 class AdminMailer < ActionMailer::Base
 
-  default :from => "contact@travelercar.com", :to => "support@travelercar.com "
+  default :from => "contact@travelercar.com", :to => "htaidirt+admin@gmail.com"
+  # default :from => "contact@travelercar.com", :to => "support@travelercar.com"
 
   def rent_notification(rent)
     @user = rent.user
-    @url  = user_rent_url(rent.user, rent)
+    @url  = user_rent_url(I18n.locale, rent.user, rent)
     @admin_url  = admin_rent_url(rent)
     mail(:subject => "New rent on TravelerCar.com")
   end
 
   def travel_notification(travel)
     @user = travel.user
-    @url  = user_travel_url(travel.user, travel)
+    @url  = user_travel_url(I18n.locale, travel.user, travel)
     @admin_url  = admin_travel_url(travel)
     mail( :subject => "New parking on TravelerCar.com")
   end
