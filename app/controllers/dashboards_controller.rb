8bc82a3ab9900810eaaeca996c38c248e7866bc0
Hassen Taidirt <htaidirt@gmail.com>
2013-09-11 11:50:30 +0200

    Change dashboard information.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,10 @@ class DashboardsController < ApplicationController
 	
 	def index
 		@user = User.find_by_email current_user.email
+		@travels = @user.travels
+		@next_travel = @travels.order("departure ASC").select{ |t| t.departure > Time.now }.first if !@travels.blank?
+		@rents = @user.rents
+		@next_rent = @rents.order("startDate ASC").select{ |r| r.startDate > Time.now }.first if !@rents.blank?
 	end
 
 end
