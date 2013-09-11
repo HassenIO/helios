9eb2b6c8662f4524799eed6be546c4bd38dab2c0
Hassen Taidirt <htaidirt@gmail.com>
2013-09-11 14:19:20 +0200

    Solve wrong default_url_options to email setup.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,5 @@ TravelerCar::Application.configure do
 	config.assets.debug = true
 	config.sass.debug_info = true
 	config.sass.line_comments = false # source maps don't get output if this is true
+	
 end
