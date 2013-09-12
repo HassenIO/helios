d70f9990b6f324290aea79e92444fce18a58bdd8
Hassen Taidirt <htaidirt@gmail.com>
2013-09-12 23:05:03 +0200

    Improve datetime picker user experience by hiding when date is selected.

--- app/assets/javascripts/footer.js
+++ app/assets/javascripts/footer.js
@@ -1,2 +1,20 @@
 //= require twitter/bootstrap
 //= require argo/index
+
+$(function(){
+
+	/*
+	 *	Need to check the use conditions in order to signup.
+	 */
+	$("#signup-accept-cgu").click(function(){
+		if(this.checked) {
+			$("#signup-button").removeAttr("disabled");
+			console.log("btn enabled");
+		} else {
+			$("#signup-button").attr("disabled", "disabled");
+			console.log("btn disabled");
+		}
+	});
+	
+});
+
