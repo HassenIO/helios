2755fc14d324c0ad1c601b1fd3f3e10ea7e5cf95
Hassen Taidirt <htaidirt@gmail.com>
2013-09-12 15:44:21 +0200

    Add checkbox to check (accept legal use), in order to be able to signup.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,20 @@
 //= require jquery
 //= require jquery_ujs
 
+$(function(){
 
+	/*
+	 *	Need to check the use conditions in order to signup.
+	 */
+	$("#signup-accept-cgu").click(function(){
+		if(this.checked) {
+			$("#signup-button").removeAttr("disabled");
+			console.log("btn enabled");
+		} else {
+			$("#signup-button").attr("disabled", "disabled");
+			console.log("btn disabled");
+		}
+	});
+
+});
 
