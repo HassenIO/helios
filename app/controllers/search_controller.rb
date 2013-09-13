c57b06a33a8be9cf2185b71dbe73ecd23e45b374
Hassen Taidirt <htaidirt@gmail.com>
2013-09-13 12:19:29 +0200

    Update datetime picker plugin.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,7 @@ class SearchController < ApplicationController
 
 	def index
 		@rent = Rent.new
-		# @rent.airPort = AirPort.find(1) # DON'T UNDERSTAND WHAT IS THIS ???
+		# @rent.airPort = AirPort.find(1) # DON'T UNDERSTAND WHAT/WHY IS THIS???
 
 		respond_to do |format|
 			format.html # index.html.erb
