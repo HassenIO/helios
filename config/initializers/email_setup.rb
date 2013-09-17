e8654891eac670095882ab57d90184342afc7552
Hassen Taidirt <htaidirt@gmail.com>
2013-09-17 16:34:56 +0200

    Change email settings.

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -1,40 +1,49 @@
 ActionMailer::Base.default_url_options = { :host => ENV['HOST'], :locale => I18n.locale }
 
-if Rails.env.production?
+# if Rails.env.production?
 
-	# We are in production mode -> Use 1and1 SMTP server
-	ActionMailer::Base.smtp_settings = {
-		:address        => "auth.smtp.1and1.fr",
-		:port           => "587",
-		:authentication => :plain,
-		:user_name      => ENV['1N1_USERNAME'],
-		:password       => ENV['1N1_PASSWORD']
-	}
+# 	# We are in production mode -> Use 1and1 SMTP server
+# 	ActionMailer::Base.smtp_settings = {
+# 		:address        => "auth.smtp.1and1.fr",
+# 		:port           => "587",
+# 		:authentication => :plain,
+# 		:user_name      => ENV['1N1_USERNAME'],
+# 		:password       => ENV['1N1_PASSWORD']
+# 	}
 
-else
+# else
 
-	# We are in dev/test mode -> Use Gmail SMTP Server
-	ActionMailer::Base.smtp_settings = {
-		:address => "smtp.gmail.com",
-		:port => 587,
-		:user_name => "test.smtp@adventori.com",
-		:password => "smtp;test",
-		:authentication => "plain",
-		:enable_starttls_auto => true
-	}
+# 	# We are in dev/test mode -> Use Gmail SMTP Server
+# 	ActionMailer::Base.smtp_settings = {
+# 		:address => "smtp.gmail.com",
+# 		:port => 587,
+# 		:user_name => "test.smtp@adventori.com",
+# 		:password => "smtp;test",
+# 		:authentication => "plain",
+# 		:enable_starttls_auto => true
+# 	}
 
-end
+# end
 
-# In dev env, we create fake users for test
-# So, we redirect all mails to these users, into a valid email address
-if Rails.env.development?
-	class OverrideMailRecipient
-		def self.delivering_email(mail)
-			mail.to = "admin@travelercar.com"
-		end
-	end
-	AdminMailer.register_interceptor(OverrideMailRecipient)
-end
+# We are in production mode -> Use 1and1 SMTP server
+ActionMailer::Base.smtp_settings = {
+	:address        => "auth.smtp.1and1.fr",
+	:port           => "587",
+	:authentication => :plain,
+	:user_name      => ENV['1N1_USERNAME'],
+	:password       => ENV['1N1_PASSWORD']
+}
+
+# # In dev env, we create fake users for test
+# # So, we redirect all mails to these users, into a valid email address
+# if Rails.env.development?
+# 	class OverrideMailRecipient
+# 		def self.delivering_email(mail)
+# 			mail.to = "admin@travelercar.com"
+# 		end
+# 	end
+# 	AdminMailer.register_interceptor(OverrideMailRecipient)
+# end
 
 
 
