6ad01959bf607e878a9468be5a73f03e533e37de
Hassen Taidirt <htaidirt@gmail.com>
2013-09-17 14:16:31 +0200

    Reverse previous commit (doesn't work on production)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,14 +13,14 @@ TravelerCar::Application.configure do
 	config.serve_static_assets = false
 
 	# Compress JavaScripts and CSS
-	config.assets.compress = false
+	config.assets.compress = true
 
 	# Don't fallback to assets pipeline if a precompiled asset is missed
 	# Nota: Set to true to avoid Heroku error (source: http://stackoverflow.com/questions/7376966/heroku-problem-the-page-you-were-looking-for-doesnt-exist)
-	config.assets.compile = false
+	config.assets.compile = true
 
 	# Generate digests for assets URLs
-	config.assets.digest = false
+	config.assets.digest = true
 
 	# Defaults to nil and saved in location specified by config.assets.prefix
 	# config.assets.manifest = YOUR_PATH
