f1835cb1e996ee3c399ab304548b45fb70ec6fc9
Hassen Taidirt <htaidirt@gmail.com>
2013-09-30 16:37:37 +0200

    Remove Invitation Code checking before displaying sign up page.
    At this point, the Signup process works like a charm.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
 	# Signup page
-	# Change from default to add the invitation code gate.
 	def new
-		invitation = Invitation.get_invitation params[:code]
-
-		if invitation.nil?
-			redirect_to new_user_session_path, alert: "Le code invitation #{params[:code]} est invalide."
-		else
-			flash.now[:notice] = "Bienvenue #{invitation.desc}. Nous sommes impatients de vous compter parmi nos membres."
-		end
 	end
 
 	# PUT /resource
