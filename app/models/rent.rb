b9d3a874d8541dc4c816052865b281709db9315c
Hassen Taidirt <htaidirt@gmail.com>
2013-10-03 12:45:15 +0200

    Change some PayPal credentials.

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -80,7 +80,7 @@ class Rent < ActiveRecord::Base
 
 	def paypal_url redirect_url, notify_url
 		values = {
-		    business: 'ibnbadis74-facilitator@yahoo.com',
+		    business: 'admin-facilitator@travelercar.com',
 		    cmd: '_cart',
 		    upload: 1,
 		    return: redirect_url,
