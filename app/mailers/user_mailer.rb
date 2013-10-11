9b31c5364a1967c90b66e56e41c54c59c858deae
Hassen Taidirt <htaidirt@gmail.com>
2013-10-11 10:15:54 +0200

    Create Welcome Email message when user confirm his email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,11 +7,17 @@ class UserMailer < ActionMailer::Base
 
 
 
-	# Notify the admin about a new carpark request
+	# Congratulate the new user after email confirmation or facebook signup.
+	def welcome user
+		@user = user
+		mail to: @user.email, subject: "Bienvenue sur TravelerCar"
+	end
+
+	# Notify the user about a new carpark request
 	def travel_notification travel
 		@travel = travel
 		@user = travel.user
-		mail( to: @user.email, subject: "Votre demande de parking sur TravelerCar" )
+		mail to: @user.email, subject: "Votre demande de parking sur TravelerCar"
 	end
 
 end
