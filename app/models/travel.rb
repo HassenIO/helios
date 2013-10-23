2b53f31ec1fc68555b646ef8439a4be1540ea9a4
Hassen Taidirt <htaidirt@gmail.com>
2013-10-23 14:02:30 +0200

    Introduce Flight numbers (departure and return).

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -41,7 +41,8 @@ class Travel < ActiveRecord::Base
 	belongs_to :airPort
 
 	attr_accessible :arrival, :arrival_time, :arrival_date, :departure, :departure_date, :departure_time,
-									:car_attributes, :airPort_id, :status, :has_accepted_cgv, :user_id, :commercial_text
+					:car_attributes, :airPort_id, :status, :has_accepted_cgv, :user_id, :commercial_text,
+					:flight_n_departure, :flight_n_arrival
 
 	# add the accessors for the two fields
 	attr_accessor :departure_date, :departure_time, :arrival_time, :arrival_date
