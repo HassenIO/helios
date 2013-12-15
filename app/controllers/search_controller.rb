84c838811806bd051c7cc63f40a93dd4a563daa2
Hassen Taidirt <htaidirt@gmail.com>
2013-12-15 22:43:40 +0100

    Create Liligo XML response.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -45,8 +45,15 @@ class SearchController < ApplicationController
 	end
 
 	def api_search
+		airports = { "CDG" => 1, "ORY" => 2, "BVA" => 3 }
+		@nb_days = ((params[:dropoff_date].to_time - params[:pickup_date].to_time)/1.day).ceil
+		@travels = Travel.where('arrival > :end AND departure < :start AND "airPort_id" = :airPort_id AND status = 1',
+										{:start => params[:pickup_date],
+											:end => params[:dropoff_date],
+											:airPort_id => airports[params[:airport_id]] })
+
 		respond_to do |format|
-			format.xml { render file: "search/api/liligo", content_type: "application/xml" }
+			format.xml { render file: "search/api/#{params[:affiliate_id]}", content_type: "application/xml" }
 		end
 	end
 
