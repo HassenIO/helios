a03fcc97a8a7e468523ebbfcf2a064723f61bcae
Hassen Taidirt <htaidirt@gmail.com>
2014-01-13 01:45:31 +0100

    Add next departures/arrivals dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -50,7 +50,22 @@ ActiveAdmin.register_page "Dashboard" do
 			end
 
 			column do
-				
+				panel "Next departures" do
+					table_for Travel.where("rdv > '#{Time.now}' AND rdv <= '#{Time.now + 7.days}' AND status = 1").each do |travel|
+						column(:id) { |travel| link_to("Travel ##{travel.id}", admin_travel_path(travel.id)) }
+						column(:airport) { |travel| travel.airPort.name }
+						column(:user) { |travel| link_to travel.user.name, admin_user_path(travel.user) }
+						column("Date/Time") { |travel| status_tag travel.rdv.strftime("%a %d %b - %H:%M"), ((travel.rdv > Time.now + 2.days)? :active : :canceled) }
+					end
+				end
+				panel "Next returns" do
+					table_for Travel.where("arrival > '#{Time.now}' AND arrival <= '#{Time.now + 7.days}' AND status = 1").each do |travel|
+						column(:id) { |travel| link_to("Travel ##{travel.id}", admin_travel_path(travel.id)) }
+						column(:airport) { |travel| travel.airPort.name }
+						column(:user) { |travel| link_to travel.user.name, admin_user_path(travel.user) }
+						column("Date/Time") { |travel| status_tag travel.arrival.strftime("%a %d %b - %H:%M"), ((travel.arrival > Time.now + 2.days)? :active : :canceled) }
+					end
+				end
 				panel "Last users" do
 					table_for User.order('id desc').limit(10).each do |user|
 						column(:email)    {|user| link_to(user.email, admin_user_path(user)) }
