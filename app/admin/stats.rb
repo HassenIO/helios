b619547011bae4ba3686eb040a272442c24ca767
Hassen Taidirt <htaidirt@gmail.com>
2014-02-13 21:04:42 +0100

    More API stats

--- app/admin/stats.rb
+++ app/admin/stats.rb
@@ -59,9 +59,9 @@ ActiveAdmin.register User, as: "Stats" do
 
 		ul do
 			li "7 jours et moins : #{days_7} (#{ (days_7*100/count).ceil }%)"
-			li "entre 8 et 15 jours : #{days_15} (#{ (days_15*100/count).ceil }%)"
-			li "entre 16 et 30 jours : #{days_30} (#{ (days_30*100/count).ceil }%)"
-			li "strictement plus de 30 jours : #{days_999} (#{ (days_999*100/count).ceil }%)"
+			li "Entre 8 et 15 jours : #{days_15} (#{ (days_15*100/count).ceil }%)"
+			li "Entre 16 et 30 jours : #{days_30} (#{ (days_30*100/count).ceil }%)"
+			li "Strictement plus de 30 jours : #{days_999} (#{ (days_999*100/count).ceil }%)"
 		end
 
 		br
@@ -73,7 +73,7 @@ ActiveAdmin.register User, as: "Stats" do
 		days_30 = 0
 		days_999 = 0
 		Travel.all.each do |t|
-			d = (t.arrival.to_date - t.departure.to_date).to_i + 1
+			d = (t.departure.to_date - t.created_at.to_date).to_i + 1
 			days_3 += 1 if d <= 3
 			days_7 += 1 if (d > 3 && d <= 7)
 			days_15 += 1 if (d > 7 && d <= 15)
@@ -83,10 +83,10 @@ ActiveAdmin.register User, as: "Stats" do
 
 		ul do
 			li "3 jours et moins : #{days_3} (#{ (days_3*100/count).ceil }%)"
-			li "entre 4 et 7 jours : #{days_7} (#{ (days_7*100/count).ceil }%)"
-			li "entre 8 et 15 jours : #{days_15} (#{ (days_15*100/count).ceil }%)"
-			li "entre 16 et 30 jours : #{days_30} (#{ (days_30*100/count).ceil }%)"
-			li "strictement plus de 30 jours : #{days_999} (#{ (days_999*100/count).ceil }%)"
+			li "Entre 4 et 7 jours : #{days_7} (#{ (days_7*100/count).ceil }%)"
+			li "Entre 8 et 15 jours : #{days_15} (#{ (days_15*100/count).ceil }%)"
+			li "Entre 16 et 30 jours : #{days_30} (#{ (days_30*100/count).ceil }%)"
+			li "Strictement plus de 30 jours : #{days_999} (#{ (days_999*100/count).ceil }%)"
 		end
 
 
@@ -126,7 +126,7 @@ ActiveAdmin.register User, as: "Stats" do
 
 
 		br
-		h3 "Clics par affilié"
+		h3 "Clics à travers l'API par affilié"
 
 		requests = ApiMgt.group(:affiliate_id, :airport_id).sum(:nb_clicks)
 		total_clicks = 0
@@ -138,6 +138,73 @@ ActiveAdmin.register User, as: "Stats" do
 				total_clicks += count
 			end
 			li "Total : #{total_clicks} clics"
+		end
+
+
+		br
+		h3 "Anticipation des recherches via l'API"
+
+		days_2 = 0
+		days_7 = 0
+		days_15 = 0
+		days_30 = 0
+		days_60 = 0
+		days_999 = 0
+		ApiMgt.all.each do |r|
+			d = ((Time.strptime( "#{r.pickup_date} #{r.pickup_time}", "%Y-%m-%d %H:%M" ) - r.created_at)/1.day).ceil
+			days_2 += 1 if d <= 2
+			days_7 += 1 if (d > 2 && d <= 7)
+			days_15 += 1 if (d > 7 && d <= 15)
+			days_30 += 1 if (d > 15 && d <= 30)
+			days_60 += 1 if (d > 30 && d <= 60)
+			days_999 += 1 if (d > 60)
+		end
+
+		ul do
+			li "2 jours et moins : #{days_2} (#{ (100 * days_2 / count_requests).ceil }%)"
+			li "Entre 3 et 7 jours : #{days_7} (#{ (100 * days_7 / count_requests).ceil }%)"
+			li "Entre 8 et 15 jours : #{days_15} (#{ (100 * days_15 / count_requests).ceil }%)"
+			li "Entre 16 et 30 jours : #{days_30} (#{ (100 * days_30 / count_requests).ceil }%)"
+			li "Entre 31 et 60 jours : #{days_60} (#{ (100 * days_60 / count_requests).ceil }%)"
+			li "Strictement plus de 60 jours : #{days_999} (#{ (100 * days_999 / count_requests).ceil }%)"
+		end
+
+
+		br
+		h3 "Répartition des durée de locations via l'API"
+
+		days_1 = 0
+		days_2 = 0
+		days_3 = 0
+		days_4 = 0
+		days_7 = 0
+		days_10 = 0
+		days_15 = 0
+		days_21 = 0
+		days_999 = 0
+		ApiMgt.all.each do |r|
+			d = ((Time.strptime( "#{r.dropoff_date} #{r.dropoff_time}", "%Y-%m-%d %H:%M" ) - Time.strptime( "#{r.pickup_date} #{r.pickup_time}", "%Y-%m-%d %H:%M" ))/1.day).ceil
+			days_1 += 1 if d <= 1
+			days_2 += 1 if d == 2
+			days_3 += 1 if d == 3
+			days_4 += 1 if d == 4
+			days_7 += 1 if (d > 4 && d <= 7)
+			days_10 += 1 if (d > 7 && d <= 10)
+			days_15 += 1 if (d > 10 && d <= 15)
+			days_21 += 1 if (d > 15 && d <= 21)
+			days_999 += 1 if d > 21
+		end
+
+		ul do
+			li "1 jour et moins : #{days_1} (#{ (100 * days_1 / count_requests).ceil }%)"
+			li "2 jours et moins : #{days_2} (#{ (100 * days_2 / count_requests).ceil }%)"
+			li "3 jours et moins : #{days_3} (#{ (100 * days_3 / count_requests).ceil }%)"
+			li "4 jours et moins : #{days_4} (#{ (100 * days_4 / count_requests).ceil }%)"
+			li "Entre 5 et 7 jours : #{days_7} (#{ (100 * days_7 / count_requests).ceil }%)"
+			li "Entre 8 et 10 jours : #{days_10} (#{ (100 * days_10 / count_requests).ceil }%)"
+			li "Entre 11 et 15 jours : #{days_15} (#{ (100 * days_15 / count_requests).ceil }%)"
+			li "Entre 16 et 21 jours : #{days_21} (#{ (100 * days_21 / count_requests).ceil }%)"
+			li "Strictement plus de 21 jours : #{days_999} (#{ (100 * days_999 / count_requests).ceil }%)"
 		end
 
 	end
